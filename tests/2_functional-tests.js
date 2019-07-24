c347eb581c7bd579771de1be248be3b87a48535e
SL477 <fishwickt@gmail.com>
2019-07-24 21:40:59 +0100

    Tests complete

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -24,25 +24,62 @@ suite('Functional Tests', function() {
         .end(function(err, res){
           
           //complete this one too
-          
+          assert.equal(res.status, 200);
+          //console.log(res.body);
+          assert.equal(res.body.stockData.stock, 'goog');
+          assert.isNumber(res.body.stockData.price);
+          assert.isNumber(res.body.stockData.likes);
           done();
         });
       });
       
       test('1 stock with like', function(done) {
-        
+        chai.request(server)
+        .get('/api/stock-prices')
+        .query({stock: 'goog', like: true})
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.stockData.stock, 'goog');
+          assert.isNumber(res.body.stockData.price);
+          assert.isNumber(res.body.stockData.likes);
+          done();
+        });
       });
       
       test('1 stock with like again (ensure likes arent double counted)', function(done) {
-        
+        chai.request(server)
+        .get('/api/stock-prices')
+        .query({stock: 'goog', like: true})
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.stockData.stock, 'goog');
+          assert.isNumber(res.body.stockData.price);
+          assert.isNumber(res.body.stockData.likes);
+          done();
+        });
       });
       
       test('2 stocks', function(done) {
-        
+        chai.request(server)
+        .get('/api/stock-prices')
+        .query({stock: ['goog','msft']})
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          console.log(res.body.stockData);
+          assert.isArray(res.body.stockData);
+          done();
+        });
       });
       
       test('2 stocks with like', function(done) {
-        
+        chai.request(server)
+        .get('/api/stock-prices')
+        .query({stock: ['goog','msft'], like: true})
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.isArray(res.body.stockData);
+          done();
+        });
       });
       
     });
