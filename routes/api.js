0de41c5a9911aac44127a80aafb3d2e3be126eaa
Tom Fishwick <fishwickt@gmail.com>
2024-03-24 19:00:57 +0000

    Started turning into a module

--- routes/api.js
+++ routes/api.js
@@ -1,253 +1,140 @@
 /*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
 'use strict';
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb').MongoClient;
-var Mongoose = require('mongoose');
+import { Schema, model, connect } from 'mongoose';
+import { config } from 'dotenv';
+config();
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
-//HTTP
-const https = require('https');
-let options = {
-  host: 'https://financialmodelingprep.com',
-  path: '/api/v3/stock/real-time-price/'/*,
-  headers: {
-    'Content-Type': 'application/json'
-  }*/
-};
+// HTTP
+import { get } from 'https';
+// let options = {
+//   host: 'https://financialmodelingprep.com',
+//   path: '/api/v3/stock/real-time-price/'/*,
+//   headers: {
+//     'Content-Type': 'application/json'
+//   }*/
+// };
 
-//Schema
-var stockSchema = new Mongoose.Schema({
-  ipAddr: { type: String, required: true},
-  stockCode: { type: String, required: true },
-  like: { type: Number, required: true }
+// Schema
+const stockSchema = new Schema({
+    ipAddr: { type: String, required: true },
+    stockCode: { type: String, required: true },
+    like: { type: Number, required: true },
 });
 
-var stockrecord = Mongoose.model('stockrecords', stockSchema);
+const stockRecord = model('stockrecords', stockSchema);
 
-module.exports = function (app) {
-  Mongoose.connect(CONNECTION_STRING, { useNewUrlParser: true, useFindAndModify: false }, function (err) {
-    if (err) {
-      console.log(err);
-      res.send(err);
-    } else {
-      console.log('db connected');
-    }});
+export default function (app) {
+    connect(CONNECTION_STRING)
+        .then(console.log('db connected'))
+        .catch((err) => console.error(err));
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      let stockDataArr = [];
-      if (Array.isArray(req.query.stock)) {
-        req.query.stock.forEach(element => {
-          stockDataArr.push({'stock': element});
-        });
-      } else {
-        stockDataArr.push({'stock': req.query.stock});
-      }
-
-      //Get the number of stocks in the array and keep count
-      var cnt = stockDataArr.length;
-      console.log(cnt);
-      var i = 0;
-
-      stockDataArr.forEach(element => {
-        i++;
-        //if like then check if the stock and IP exist in the collection
-        if (req.query.like) {
-          stockrecord.find({'stockCode': element.stock, 'ip': req.ip}, function (err, doc) {
-            if (err) {
-              console.log(err);
-            } else {
-              if (!doc) {
-                stockrecord.create({'ipAddr': req.ip, 'stockCode': element.stock, 'like': 1}, function (err) {
-                  if (err) {
-                    console.log(err);
-                  } else {
-                    getLikes(req, res, stockDataArr, element, cnt, i);
-                  }
-                });
-              } else {
-                getLikes(req, res, stockDataArr, element, cnt, i);
-              }
-            }
-          });
-        } else {
-          getLikes(req, res, stockDataArr, element, cnt, i);
-        }
-      });
-
-      /*let stock = req.query.stock;
-      let stocker = '';
-      if (Array.isArray(stock)) {
-        let delim = '';
-        req.query.stock.forEach(element => {
-          stocker += delim + element;
-          delim = ',';
-
-          //if like then check if the stock and IP exist in the collection
-        if (req.query.like) {
-            stockrecord.find({stockCode: element, ip: req.ip}, function (err, doc) {
-              if (err) {
-                console.log(err);
-              } else {
-                if (!doc) {
-                  stockrecord.create({'ipAddr': req.ip, 'stockCode': element, 'like': 1}, function(err) {
-                    if (err) {
-                      console.log(err);
-                    } else {
-                      console.log('inserted into db');
-                    }
-                  });
-                } else {
-                  console.log('ip ' + req.ip + ' already used.');
-                }
-              }
+    app.route('/api/stock-prices').get(function (req, res) {
+        const stockDataArr = [];
+        if (Array.isArray(req.query.stock)) {
+            req.query.stock.forEach((element) => {
+                stockDataArr.push({ stock: element });
             });
-          }
-          //Get the number of likes
-          var likes = 0;
-          stockrecord.find({stockCode: element}, function (err, docs) {
-            if (err) {
-              console.log(err);
-            } else {
-              docs.forEach(ele => {
-                like += 1;
-              });
-              var stockrec = {'stock': element, 'price': 0, 'likes': likes};
-              console.log(stockrec);
-              stockDataArr.push(stockrec);
-            }
-          });
-         
-        });
-      } else {
-        stocker = stock;
-         //if like then check if the stock and IP exist in the collection
-         if (req.query.like) {
-          stockrecord.find({stockCode: stock, ipAddr: req.ip}, function (err, doc) {
-            if (err) {
-              console.log(err);
-            }
-            if (!doc || doc.length == 0) {
-              stockrecord.create({'ipAddr': req.ip, 'stockCode': stock, 'like': 1}, function(err) {
-                if (err) {
-                  console.log(err);
-                } else {
-                  console.log('inserted into db');
-                }
-              });
-            } else {
-              console.log(doc);
-              console.log('ip ' + req.ip + ' already used.');
-            }
-          });
+        } else {
+            stockDataArr.push({ stock: req.query.stock });
         }
 
-         //Get the number of likes
-         var likes = 0;
-         stockrecord.find({'stockCode': stock}, function (err, docs) {
-           if (err) {
-             console.log(err);
-           } else {
-             docs.forEach(ele => {
-               likes += 1;
-               console.log(ele);
-               
-             });
-             var stockrec = {'stock': stock, 'price': 0, 'likes': likes};
-             console.log(stockrec);
-           }
-         });
-         
-      }
-      console.log(req.ip);
-      //https://financialmodelingprep.com/api/v3/stock/real-time-price/AAPL
-      console.log(stock);
-      let like = req.query.like;
-      console.log(like);
-      let output = '';
-      //console.log(process.env.STOCKAPI);
-      let request = https.get(process.env.STOCKAPI + stocker, function (result) {
-        //console.log(result);
-        result.setEncoding('utf8');
-        result.on('data', (chunk) => {
-          output += chunk;
-          console.log(chunk);
+        // Get the number of stocks in the array and keep count
+        let cnt = stockDataArr.length;
+        console.log(cnt);
+        let i = 0;
+
+        stockDataArr.forEach((element) => {
+            i++;
+            // if like then check if the stock and IP exist in the collection
+            if (req.query.like) {
+                stockRecord
+                    .find({ stockCode: element.stock, ip: req.ip })
+                    .then((doc) => {
+                        if (!doc) {
+                            stockRecord
+                                .create({
+                                    ipAddr: req.ip,
+                                    stockCode: element.stock,
+                                    like: 1,
+                                })
+                                .then(
+                                    getLikes(
+                                        req,
+                                        res,
+                                        stockDataArr,
+                                        element,
+                                        cnt,
+                                        i
+                                    )
+                                )
+                                .catch((err) => console.error(err));
+                        } else {
+                            getLikes(req, res, stockDataArr, element, cnt, i);
+                        }
+                    })
+                    .catch((err) => console.error(err));
+            } else {
+                getLikes(req, res, stockDataArr, element, cnt, i);
+            }
         });
-        
-        res.on('end', () => {
-          let obj = JSON.parse(output);
-          console.log(obj);
-        });
-      });
-      console.log(output);
-      res.json({'stock': stock, 'price': 0, 'likes': like});*/
     });
 
     function getLikes(req, res, stockDataArr, element, cnt, i) {
-      //Get the number of likes. Then get the stock prices, if i = cnt call the done function and return.
-      element['price'] = 0;
-      element['likes'] = 0;
-      stockrecord.find({'stockCode': element.stock}, function (err, docs) {
-        if (err) {
-          console.log(err);
-        } else {
-          if (!docs) {
-            getPrice(req, res, stockDataArr, element, cnt, i);
-          } else {
-            docs.forEach(ele => {
-              element.likes += ele.like;
-          });
-          getPrice(req, res, stockDataArr, element, cnt, i);
-          };
-        }
-      });
-    };
+        // Get the number of likes. Then get the stock prices, if i = cnt call the done function and return.
+        element['price'] = 0;
+        element['likes'] = 0;
+        stockRecord
+            .find({ stockCode: element.stock })
+            .then((docs) => {
+                if (!docs) {
+                    getPrice(req, res, stockDataArr, element, cnt, i);
+                } else {
+                    docs.forEach((element) => (element.likes += element.like));
+                }
+            })
+            .catch((err) => console.error(err));
+    }
 
     function getPrice(req, res, stockDataArr, element, cnt, i) {
-      let output = '';
-      let request = https.get(process.env.STOCKAPI + element.stock, function (result) {
-        //console.log(result);
-        result.setEncoding('utf8');
-        result.on('data', (chunk) => {
-          output += chunk;
-          console.log(chunk);
-          let obj = JSON.parse(chunk);
-          console.log(obj.price);
-          element.price = obj.price;
-          console.log(element);
-          if (i == cnt) {
-            //res.json({'stockData': stockDataArr});
-            done(req, res, stockDataArr, element, cnt, i);
-          }
+        get(process.env.STOCKAPI + element.stock, function (result) {
+            result.setEncoding('utf8');
+            result.on('data', (chunk) => {
+                console.log(chunk);
+                const obj = JSON.parse(chunk);
+                console.log(obj.price);
+                element.price = obj.price;
+                console.log(element);
+                if (i == cnt) {
+                    done(req, res, stockDataArr, element, cnt, i);
+                }
+            });
         });
-        
-        /*res.on('end', () => {
-          let obj = JSON.parse(output);
-          console.log(obj);
-        });*/
-      });
-    };
-
-  function done(req, res, stockDataArr, element, cnt, i) {
-    if (stockDataArr.length == 1) {
-      res.json({'stockData': stockDataArr[0]});
-    } else if (stockDataArr.length == 2){
-      stockDataArr[0]['rel_likes'] = stockDataArr[0].likes - stockDataArr[1].likes;
-      stockDataArr[1]['rel_likes'] = stockDataArr[1].likes - stockDataArr[0].likes;
-      delete stockDataArr[0]['likes'];
-      delete stockDataArr[1]['likes'];
-      res.json({'stockData': stockDataArr});
-    } else {
-      res.json({'stockData': stockDataArr});
     }
-  };
-};
+
+    // eslint-disable-next-line no-unused-vars
+    function done(req, res, stockDataArr, element, cnt, i) {
+        if (stockDataArr.length == 1) {
+            res.json({ stockData: stockDataArr[0] });
+        } else if (stockDataArr.length == 2) {
+            stockDataArr[0]['rel_likes'] =
+                stockDataArr[0].likes - stockDataArr[1].likes;
+            stockDataArr[1]['rel_likes'] =
+                stockDataArr[1].likes - stockDataArr[0].likes;
+            delete stockDataArr[0]['likes'];
+            delete stockDataArr[1]['likes'];
+            res.json({ stockData: stockDataArr });
+        } else {
+            res.json({ stockData: stockDataArr });
+        }
+    }
+}
--- server.js
+++ server.js
@@ -1,75 +1,80 @@
 'use strict';
+import { config } from 'dotenv';
+config();
+import express from 'express';
+import pkg from 'body-parser';
+const { json, urlencoded } = pkg;
+import cors from 'cors';
+import apiRoutes from './routes/api.js';
+import fccTestingRoutes from './routes/fcctesting.js';
+import runner from './test-runner.js';
 
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var expect      = require('chai').expect;
-var cors        = require('cors');
+// Helmet
+import {
+    hidePoweredBy,
+    frameguard,
+    xssFilter,
+    noSniff,
+    ieNoOpen,
+    contentSecurityPolicy,
+} from 'helmet';
 
-var apiRoutes         = require('./routes/api.js');
-var fccTestingRoutes  = require('./routes/fcctesting.js');
-var runner            = require('./test-runner');
+const app = express();
 
-//Helmet
-var helmet = require('helmet');
-
-var app = express();
-
-//Helmet
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard());
-app.use(helmet.xssFilter());
-app.use(helmet.noSniff());
-app.use(helmet.ieNoOpen());
-app.use(helmet.contentSecurityPolicy(
-  {
-    directives: {
-      defaultSrc: ["'self'"],
-      styleSrc: ["'self'"],
-    }
-  }
-));
+// Helmet
+app.use(hidePoweredBy());
+app.use(frameguard());
+app.use(xssFilter());
+app.use(noSniff());
+app.use(ieNoOpen());
+app.use(
+    contentSecurityPolicy({
+        directives: {
+            defaultSrc: ["'self'"],
+            styleSrc: ["'self'"],
+        },
+    })
+);
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(cors({ origin: '*' })); //For FCC testing purposes only
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(json());
+app.use(urlencoded({ extended: true }));
 
-//Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
+// Index page (static HTML)
+app.route('/').get(function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
-  });
+});
 
-//For FCC testing purposes
+// For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);  
-    
-//404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+// Routing for API
+apiRoutes(app);
+
+// 404 Not Found Middleware
+// eslint-disable-next-line no-unused-vars
+app.use(function (req, res, next) {
+    res.status(404).type('text').send('Not Found');
 });
 
 //Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch(e) {
-        var error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
-      }
-    }, 3500);
-  }
+const listener = app.listen(process.env.PORT || 3000, function () {
+    console.log('Listening on port ' + listener.address().port);
+    if (process.env.NODE_ENV === 'test') {
+        console.log('Running Tests...');
+        setTimeout(function () {
+            try {
+                runner.run();
+            } catch (e) {
+                var error = e;
+                console.log('Tests are not valid:');
+                console.log(error);
+            }
+        }, 3500);
+    }
 });
 
-module.exports = app; //for testing
+export default app; //for testing
